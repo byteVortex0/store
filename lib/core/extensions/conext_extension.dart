8c6da23633676bb755ae5a1944be8485fcdc1763
DESKTOP-SJCF9DP\Essam <mahmodahmed201559@gmail.com>
2024-11-17 14:44:11 +0200

    editing themes

--- lib/core/extensions/conext_extension.dart
+++ lib/core/extensions/conext_extension.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 extension ConextExtension on BuildContext {
+
+  //Color
+  MyColors get color => Theme.of(this).extension<MyColors>()!;
+
+  //Images
+  MyImages get image => Theme.of(this).extension<MyImages>()!;
+
+  //pages
   Future<dynamic> pushName(String routeName, {Object? arg}) =>
       Navigator.of(this).pushNamed(routeName, arguments: arg);
 
