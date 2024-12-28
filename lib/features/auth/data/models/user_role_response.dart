5df8416807f7a087ea7b47798fe08bc8a1253d3b
DESKTOP-SJCF9DP\Essam <mahmodahmed201559@gmail.com>
2024-12-28 02:12:31 +0200

    login model done

---
+++ lib/features/auth/data/models/user_role_response.dart
@@ -0,0 +1,15 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_role_response.g.dart';
+
+@JsonSerializable()
+class UserRoleResponse {
+  @JsonKey(name: 'role')
+  final String? userRole;
+
+  UserRoleResponse({required this.userRole});
+
+  factory UserRoleResponse.fromJson(Map<String, dynamic> json) =>
+      _$UserRoleResponseFromJson(json);
+}
